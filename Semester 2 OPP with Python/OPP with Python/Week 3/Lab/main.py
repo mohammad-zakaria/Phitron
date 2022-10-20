5a5ceaf6b7e0e8f4f2ae37929bfa45a5eb8f6954
Zack <zakaria.eee.iiuc@gmail.com>
2022-10-20 14:45:30 +0600

    added lab exercise of week 3

--- Semester 2 OPP with Python/OPP with Python/Week 3/Lab/main.py
+++ Semester 2 OPP with Python/OPP with Python/Week 3/Lab/main.py
@@ -30,9 +30,17 @@ class CameraApp(QWidget):
         self.img_width = 640
         self.img_height = 400
 
+        # other variable
+        self.dt = '0-0-0'
+        self.record_flag = False
+
         #load icon
         self.camera_icon = QIcon(cam_icon_path)
         self.rec_icon = QIcon(rec_icon_path)
+        self.stop_icon = QIcon(stop_icon_path)
+
+        #to save the video
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
         #set up main window
         self.setWindowTitle('My Camera App')
@@ -70,7 +78,7 @@ class CameraApp(QWidget):
 
         #record button
         self.record_btn = QPushButton(self)
-        self.record_btn.setIcon(self.rec_icon)
+        # self.record_btn.setIcon(self.rec_icon)
         self.record_btn.setStyleSheet('border-radius: 30px; border: 2px solid black; border-width: 2px; padding: 3px;')
         self.record_btn.setFixedSize(60, 60)
         self.record_btn.clicked.connect(self.record)
@@ -91,6 +99,14 @@ class CameraApp(QWidget):
     def update(self):
         """ update frame """
         _,self.frame = self.cap.read()
+
+        if self.record_flag == True:
+            print('Recording..')
+            self.record_btn.setIcon(self.stop_icon)
+            self.frame = cv2.circle(self.frame,(20, 70),5, (0,0,255), 10)
+        else:
+            self.record_btn.setIcon(self.rec_icon)
+
         frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
         height, width, channel = frame.shape
         step = channel * width
@@ -106,7 +122,19 @@ class CameraApp(QWidget):
 
     def record(self):
         """ record video """
-        print('Recording')
+        print(self.record_flag)
+
+        if self.record_flag == True:
+            self.record_flag = False
+            print('Stop Recording')
+        else :
+            self.record_flag = True
+            print('Start Recording')
+            self.get_time()
+            
+            self.out = cv2.VideoWriter(f'{self.dt}.avi', self.fourcc, 20.0, (640, 480))
+
+
     
 
     def get_time(self):
@@ -116,9 +144,11 @@ class CameraApp(QWidget):
         print(self.dt)
 
 # run app
-cam_icon_path = 'assests/capture.png'
-rec_icon_path = 'assests/video.png'
+if __name__ == '__main__':
+    cam_icon_path = 'assests/capture.png'
+    rec_icon_path = 'assests/video.png'
+    stop_icon_path = 'assests/stop.png'
 
-app = QApplication(sys.argv)
-win = CameraApp()
-sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    win = CameraApp()
+    sys.exit(app.exec_())
